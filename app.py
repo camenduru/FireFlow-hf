566e04b431ddec774365aea4f1faa1a29b9618ad
Jiangshan Wang <wjs0725@users.noreply.huggingface.co>
2024-11-30 06:08:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ class FluxEditor:
         self.model.eval()
     
     @torch.inference_mode()
-    @spaces.GPU(duration=30)
+    @spaces.GPU(duration=180)
     def edit(self, init_image, source_prompt, target_prompt, num_steps, inject_step, guidance, seed):
         torch.cuda.empty_cache()
         seed = None
