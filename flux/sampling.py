4a18092be98bb07e177b835c03e8e7de5566c089
Jiangshan Wang <wjs0725@users.noreply.huggingface.co>
2024-11-30 07:35:20 +0000

    Update flux/sampling.py

--- flux/sampling.py
+++ flux/sampling.py
@@ -32,7 +32,7 @@ def prepare(t5: HFEmbedder, clip: HFEmbedder, img: Tensor, prompt: str | list[st
 
     print("!!!!!!!!!!!!opts.source_prompt!!!!!!!!!!!!",len(prompt))
     print("!!!!!!!!!!!!opts.source_prompt!!!!!!!!!!!!",prompt)
-    print("!!!!!!!!!!clip!!!!!!!!!",device = next(clip.parameters()).device)
+    print("!!!!!!!!!!clip!!!!!!!!!",next(clip.parameters()).device)
     vec = clip(prompt)
     if vec.shape[0] == 1 and bs > 1:
         vec = repeat(vec, "1 ... -> bs ...", bs=bs)
